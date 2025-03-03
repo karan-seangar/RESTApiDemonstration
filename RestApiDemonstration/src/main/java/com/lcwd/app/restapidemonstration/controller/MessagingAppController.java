c39e8b71d825d2ffa19a09e02d18d247e94746a5
karan-seangar <seangarkaran@gmail.com>
2025-03-03 14:52:50 +0530

    [karan seangar] added param to get name

--- RestApiDemonstration/src/main/java/com/lcwd/app/restapidemonstration/controller/MessagingAppController.java
+++ RestApiDemonstration/src/main/java/com/lcwd/app/restapidemonstration/controller/MessagingAppController.java
@@ -1,6 +1,7 @@
 package com.lcwd.app.restapidemonstration.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,11 @@ public class MessagingAppController {
 
     @GetMapping("/hello/query")
     public String hello(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz!";
+    }
+
+    @GetMapping("/hello/param/{name}")
+    public String helloParam(@PathVariable String name) {
         return "Hello " + name + " from BridgeLabz!";
     }
 }
