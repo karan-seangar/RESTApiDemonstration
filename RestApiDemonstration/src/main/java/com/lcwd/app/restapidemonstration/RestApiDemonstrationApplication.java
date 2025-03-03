b865b4341076152778c9fbad9d7c5f66a7768536
karan-seangar <seangarkaran@gmail.com>
2025-03-03 14:33:54 +0530

    [karan seangar] added get /home

---
+++ RestApiDemonstration/src/main/java/com/lcwd/app/restapidemonstration/RestApiDemonstrationApplication.java
@@ -0,0 +1,13 @@
+package com.lcwd.app.restapidemonstration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiDemonstrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiDemonstrationApplication.class, args);
+    }
+
+}
---
+++ RestApiDemonstration/src/test/java/com/lcwd/app/restapidemonstration/RestApiDemonstrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcwd.app.restapidemonstration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiDemonstrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
